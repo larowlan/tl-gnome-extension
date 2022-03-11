aae8bfbc57a0062fd81b08666194fa12647045e8
Lee Rowlands <lee.rowlands@previousnext.com.au>
2022-03-12 07:23:56 +1000

    Working version

--- extension.js
+++ extension.js
@@ -1,8 +1,11 @@
+'use strict';
+
 const St = imports.gi.St;
 const Clutter = imports.gi.Clutter;
 const Main = imports.ui.main;
 const GLib = imports.gi.GLib;
 const Lang = imports.lang;
+const GObject = imports.gi.GObject;
 const Mainloop = imports.mainloop;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
@@ -10,11 +13,13 @@ const MessageTray = imports.ui.messageTray;
 
 let button;
 
-const TlCommand = new Lang.Class({
-  Name: 'TlCommand',
-  Extends: PopupMenu.PopupBaseMenuItem,
-  _init: function(icon, text, callback, params) {
-    this.parent(params);
+const TlCommand = GObject.registerClass(
+  {
+      GTypeName: 'TlCommand'
+  },
+class TlCommand extends PopupMenu.PopupBaseMenuItem {
+  _init(icon, text, callback, params) {
+    super._init(params);
     this.actor.add_child(new St.Icon({
       icon_name: icon,
       icon_size: '22'
@@ -25,36 +30,32 @@ const TlCommand = new Lang.Class({
   }
 });
 
-const TlButton = new Lang.Class({
-  Name: 'TlButton',
+const TlButton = GObject.registerClass(
+  {
+      GTypeName: 'TlButton'
+  },class TlButton extends PanelMenu.Button {
 
-  _text: null,
-
-  Extends: PanelMenu.Button,
-
-  _timeout: undefined,
-
-  _notify: function(text) {
-    let source = new MessageTray.Source('tl', 'document-open-recent-symbolic');
+  _notify (text) {
+    const source = new MessageTray.Source('tl', 'document-open-recent-symbolic');
     Main.messageTray.add(source);
-    let notification = new MessageTray.Notification(source, text, '');
+    const notification = new MessageTray.Notification(source, text, '');
     notification.setTransient(true);
-    source.notify(notification);
-  },
+    source.showNotification(notification);
+  }
 
-  _stop: function() {
-    let text = this._execute(['stop']);
+  _stop () {
+    const text = this._execute(['stop']);
     this._notify(text);
     this._updateText();
-  },
+  }
 
-  _continue: function() {
-    let text = this._execute(['continue']);
+  _continue () {
+    const text = this._execute(['continue']);
     this._notify(text);
     this._updateText();
-  },
+  }
 
-  _execute: function(args) {
+  _execute (args) {
     args.unshift("tl");
     let out = {};
     let result = GLib.spawn_sync(null, args, null, GLib.SpawnFlags.SEARCH_PATH, null);
@@ -63,25 +64,20 @@ const TlButton = new Lang.Class({
       text = String.fromCharCode.apply(String, text);
     }
     return text;
-  },
+  }
 
-  _updateText: function() {
+  _updateText () {
     this._label.text = Lang.bind(this, this._execute)(['bitbar']);
     this._clearTimeout();
     this._timeout = Mainloop.timeout_add_seconds(15, Lang.bind(this, this._updateText));
-  },
+  }
 
-  _stopButton: null,
-  _continueButton: null,
-  _buttonMenu: null,
-  menu: null,
-
-  _init: function() {
-    this.parent(0.0);
-    this._label = new St.Label({text: "..."});
+  _init() {
+    super._init(0.0);
+    this._label = new St.Label({text: "...", style_class: 'tl__tally'});
     let icon = new St.Icon({
       icon_name: 'document-open-recent-symbolic',
-      icon_size: '22'
+      icon_size: '16'
     });
     let topBox = new St.BoxLayout();
     topBox.add_actor(icon);
@@ -102,7 +98,7 @@ const TlButton = new Lang.Class({
 
     entryNewTask.connect('key-press-event', Lang.bind(this, function(o, e) {
       let symbol = e.get_key_symbol();
-      if ((symbol == Clutter.Return) || (symbol == Clutter.KP_Enter)) {
+      if ((symbol == Clutter.KEY_Return) || (symbol == Clutter.KEY_KP_Enter)) {
         this.menu.close();
         if (o.get_text() === '') {
           return;
@@ -120,13 +116,13 @@ const TlButton = new Lang.Class({
     this._updateText();
     Main.panel.menuManager.addMenu(this.menu);
     this._timeout = Mainloop.timeout_add_seconds(15, Lang.bind(this, this._updateText));
-  },
+  }
 
-  destroy: function() {
+  destroy() {
     this._clearTimeout();
-  },
+  }
 
-  _clearTimeout: function() {
+  _clearTimeout() {
     if (this._timeout) {
       Mainloop.source_remove(this._timeout);
       this._timeout = undefined;
@@ -146,4 +142,5 @@ function enable() {
 
 function disable() {
   button.destroy();
+  button = null;
 }
